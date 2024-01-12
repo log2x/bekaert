b70ca570a4688b2711a1e0b75ef64c09f8e4e75a
Jeck Shi <jeck.shi@shinetechmagento.com>
2024-01-12 11:06:14 +0800

    add ip.sh

--- ip.sh
+++ ip.sh
@@ -2,8 +2,8 @@
 
 CURRENTTIME=`date +%Y-%m-%d-%X`
 
-URL1 = "products-applications/basic-materials/textile/conductive-fibers-and-yarns-for-smart-textiles/born-gmbh?source=fromwechat"
-URL2 = "products-applications/basic-materials/textile/conductive-fibers-and-yarns-for-smart-textiles/texible?source=fromwechat"
+URL1="products-applications/basic-materials/textile/conductive-fibers-and-yarns-for-smart-textiles/born-gmbh?source=fromwechat"
+URL2="products-applications/basic-materials/textile/conductive-fibers-and-yarns-for-smart-textiles/texible?source=fromwechat"
 
 echo "现在的时间是： ${CURRENTTIME}" >> ~/tmp/URL1_IP_Count.txt
 sudo cat /var/log/nginx/bekaert.cn.log |grep ${URL1} |awk '{print $1}' >> ~/tmp/URL1_IP_Count.txt
